8ca970479e27d44f2b480e1f5f7579c0d7aef772
Dhruvin Shah <dhruvin3@gmail.com>
2023-03-14 19:35:31 -0400

    colemak: fixing layout to proper Colemak-DH(m)

--- celeste/keymaps/colemakdh/keymap.c
+++ celeste/keymaps/colemakdh/keymap.c
@@ -22,7 +22,7 @@
 // But with this keymap it will be possible to switch between colemak and qwerty
 
 enum layers{
-    C_BASE, // colemak base
+    C_BASE, // Colemak-DH (formely know as Colemak-DHm) base, switch it to mac in order to access
     Q_BASE, // querty base
     C_FN1,  // common function 1
     C_FN2,  // common function 2
@@ -35,7 +35,7 @@ enum layers{
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [C_BASE] = LAYOUT_ansi_69(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
-        KC_TAB,  KC_Q,     KC_W,     KC_F,    KC_P,    KC_B,    KC_J,     KC_L,    KC_U,    KC_Y,    KC_QUOT,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
+        KC_TAB,  KC_Q,     KC_W,     KC_F,    KC_P,    KC_B,    KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
         KC_CAPS, KC_A,     KC_R,     KC_S,    KC_T,    KC_G,              KC_M,    KC_N,    KC_E,    KC_I,     KC_O,  KC_QUOT,  KC_ENT,           KC_HOME,
         KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_D,    KC_V,     KC_V,    KC_K,    KC_H,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
         KC_LCTL, KC_LWIN,  KC_LALT,           KC_SPC,           MO(C_FN1), MO(C_FN2),         KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
