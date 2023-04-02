9fde14e6f8d6bd04b519c5c790863a369362a178
Dhruvin Shah <dhruvin3@gmail.com>
2023-04-02 19:35:03 -0400

    right side combo added

--- celeste/keymaps/urob/keymap.c
+++ celeste/keymaps/urob/keymap.c
@@ -31,6 +31,21 @@ enum combos {
   SC_SLASH,
   TD_EQUAL,
   GV_TIDLE,
+  LU_BSPC,
+  UY_DELETE,
+  NE_BRACKETS, // on tap ( -- on hold <
+  EI_BRACKETS, // on tap ) -- on hold >
+  HC_BRACKETS, // H+, tap [ -- on hold {
+  CD_BRACKETS, // ,+. tap ] -- on hold }
+  JM_CARET,
+  LN_PLUS,
+  UE_ASTERISK,
+  YI_AMPERSAND,
+  MN_UNDERSCORE,
+  NH_MINUS,
+  EC_SLASH,
+  ID_PIPE,
+
   COMBO_LENGTH
 };
 uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define and use this instead!
@@ -50,6 +65,21 @@ const uint16_t PROGMEM sc_combo[] = {KC_S, KC_C, COMBO_END};
 const uint16_t PROGMEM td_combo[] = {KC_T, KC_D, COMBO_END};
 const uint16_t PROGMEM gv_combo[] = {KC_G, KC_V, COMBO_END};
 
+const uint16_t PROGMEM lu_combo[] = {KC_L,    KC_U,    COMBO_END};
+const uint16_t PROGMEM uy_combo[] = {KC_U,    KC_Y,    COMBO_END};
+const uint16_t PROGMEM ne_combo[] = {KC_N,    KC_E,    COMBO_END};
+const uint16_t PROGMEM ei_combo[] = {KC_E,    KC_I,    COMBO_END};
+const uint16_t PROGMEM hC_combo[] = {KC_H,    KC_COMM, COMBO_END};
+const uint16_t PROGMEM CD_combo[] = {KC_COMM, KC_DOT,  COMBO_END};
+const uint16_t PROGMEM jm_combo[] = {KC_J,    KC_M,    COMBO_END};
+const uint16_t PROGMEM ln_combo[] = {KC_L,    KC_N,    COMBO_END};
+const uint16_t PROGMEM ue_combo[] = {KC_U,    KC_E,    COMBO_END};
+const uint16_t PROGMEM yi_combo[] = {KC_Y,    KC_I,    COMBO_END};
+const uint16_t PROGMEM mn_combo[] = {KC_M,    KC_N,    COMBO_END};
+const uint16_t PROGMEM nh_combo[] = {KC_N,    KC_H,    COMBO_END};
+const uint16_t PROGMEM eC_combo[] = {KC_E,    KC_COMM, COMBO_END};
+const uint16_t PROGMEM iD_combo[] = {KC_I,    KC_DOT,  COMBO_END};
+
 combo_t key_combos[] = {
   [WF_ESC]     = COMBO(wf_combo, KC_ESC),
   [FP_ENTER]   = COMBO(fp_combo, KC_ENT),
@@ -65,6 +95,20 @@ combo_t key_combos[] = {
   [SC_SLASH]   = COMBO(sc_combo, KC_SLASH),
   [TD_EQUAL]   = COMBO(td_combo, KC_EQL),
   [GV_TIDLE]   = COMBO(gv_combo, KC_TILD),
+  [LU_BSPC]       = COMBO(lu_combo, KC_BSPC),
+  [UY_DELETE]     = COMBO(uy_combo, KC_DEL),
+  [NE_BRACKETS]   = COMBO(ne_combo, KC_LPRN),
+  [EI_BRACKETS]   = COMBO(ei_combo, KC_RPRN),
+  [HC_BRACKETS]   = COMBO(hC_combo, KC_LBRC),
+  [CD_BRACKETS]   = COMBO(CD_combo, KC_RBRC),
+  [JM_CARET]      = COMBO(jm_combo, KC_CIRC),
+  [LN_PLUS]       = COMBO(ln_combo, KC_PLUS),
+  [UE_ASTERISK]   = COMBO(ue_combo, KC_ASTR),
+  [YI_AMPERSAND]  = COMBO(yi_combo, KC_AMPR),
+  [MN_UNDERSCORE] = COMBO(mn_combo, KC_UNDS),
+  [NH_MINUS]      = COMBO(nh_combo, KC_MINUS),
+  [EC_SLASH]      = COMBO(eC_combo, KC_SLASH),
+  [ID_PIPE]       = COMBO(iD_combo, KC_PIPE),
 };
 
 
